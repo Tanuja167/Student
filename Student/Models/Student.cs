94c84fab929396dc915524a0e823ac16a7bad6f7
Tanuja167 <104304824+Tanuja167@users.noreply.github.com>
2024-06-05 00:15:56 +0530

    Add project files.

---
+++ Student/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Student.Models
+{
+
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Id { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+        [Required]
+        public int Percentage { set; get; }
+        [Required]
+        public string City { set;get; }
+    }
+}
